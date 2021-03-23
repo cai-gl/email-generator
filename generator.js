ed80cb943f383356f45c72a3305dd348082b99ae
cai-gl <81252341+cai-gl@users.noreply.github.com>
2021-03-23 11:16:52 -0500

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,177 @@
+var email_container = '';
+
+function generateiFrame() {
+    email_container = '';
+    // Array to keep track of all of the modules that will be contained in the email container
+    var email_cont_array = [];
+
+    // Build the JSON to create the preview & HTML
+    if($('.header-url').val() == '')
+    {
+        data[0].header = false;
+    }
+    else {
+        data[0].header = true;
+    }
+    if($('.footer-url').val() == '')
+    {
+        data[0].footer = false;
+    } else {
+        data[0].footer = true;
+    }
+
+    var text_elements = document.querySelectorAll('.element-container[data-type="text"]');
+    text_elements.forEach((e) => {
+        console.log('looking for data-element= ', $(e).attr('data-element'));
+        switch ($(e).attr('data-element')) {
+            case 'h2':
+                email_cont_array.push({
+                    content: `<h2>${$(e).find('.title-text').val()}</h2>`,
+                    position: $(e).attr('data-position'),
+                    type: 'text'
+                })
+                break;
+            case 'h3':
+                email_cont_array.push({
+                    content: `<h3>${$(e).find('.heading-text').val()}</h3>`,
+                    position: $(e).attr('data-position'),
+                    type: 'text'
+                })
+                break;
+            case 'h4':
+                email_cont_array.push({
+                    content: `<h4>${$(e).find('.subheading-text').val()}</h4>`,
+                    position: $(e).attr('data-position'),
+                    type: 'text'
+                })
+                break;
+            case 'p':
+                email_cont_array.push({
+                    content: `<p>${$(e).find('.paragraph-text').val()}</p>`,
+                    position: $(e).attr('data-position'),
+                    type: 'text'
+                })
+            break;
+        }
+    })
+
+    var feature_elements = document.querySelectorAll('.element-container[data-type="feature"]');
+    feature_elements.forEach((e) => {
+        switch ($(e).attr('data-element')) {
+            case 'img':
+                email_cont_array.push({
+                    content: `<tr><td style="padding: 10px"><table border="0" cellpadding="0"><tr height="100"><td class="spacer" width="${$(e).find('.image-space').val()}"></td><td style="font-family:arial,sans-serif;font-size:15px;color:#575757"><img src="${$(e).find('.image-url').val()}" width="210" height="120"><p>${$(e).find('.image-text').val()}</p></td></tr></table></td></tr>`,
+                    position: $(e).attr('data-position'),
+                    type: 'feature'
+                })
+                break;
+            case 'button':
+                email_cont_array.push({
+                    content: `<tr><td style="padding: 0 10px"><table border="0" cellpadding="0"><tr height="100"><td class="spacer" width="${$(e).find('.button-space').val()}"></td><td style="font-family:arial,sans-serif;font-size:15px;color:#575757"><table align="left" border="0" cellpadding="0" cellspacing="0" role="presentation" width="210"><tr><td style="border-radius: 4px; background: #173449; text-align: center;"><a target="_blank" href="${$(e).find('.button-url').val()}" target="_parent" style="background: #173449; border: 1px solid #173449; box-shadow: inset 0 1px 0 0 rgba(0,0,0,0); font-family: arial, sans-serif; font-size: 17px; line-height: 17px; color: #ffffff; text-decoration: none; padding: 13px 17px; display: block; border-radius: 4px; white-space: nowrap;">${$(e).find('.button-text').val()}</a></td></tr></table></td></tr></table></td></tr>`,
+                    position: $(e).attr('data-position'),
+                    type: 'feature'
+                })
+                break;
+        }
+    })
+
+    // Module text variables
+    var preview_text = $('.preview-text').val();
+    var header_image_url = $('.header-url').val();
+    var footer_image_url = $('.footer-url').val();
+
+    // HTML-based modules
+    var preview_text_container = `<div style="display: none; opacity: 0; mso-hide: all;">${preview_text}</div>`;
+
+    var header_image = `<tr><td height="115"><img alt="Learn@Cox Header" width="620" style="height:100%;width:620px;" src="${header_image_url}"></td></tr>`;
+
+    var footer_image = `<tr><td height="65"><img alt="CAI_Footer" width="620" style="width:620px;height:100%" src="${footer_image_url}"></td></tr>`;
+
+    var final_content = '';
+    var holder = '';
+
+    // Sort the email content by position
+    email_cont_array.sort((a, b) => (parseInt(a.position) > parseInt(b.position)) ? 1 : -1)
+    console.log(email_cont_array);
+    // Todo: does not work if there is only one element in the array
+    email_cont_array.forEach((el, idx) => {
+        if(idx == 0)
+        {
+            holder += el.content;
+            return;
+        }
+        // If we have an element with the same type as the previous element
+        if(el.type == email_cont_array[idx-1].type) 
+        {
+            // Push the content of the element into the holder array
+            holder += el.content;
+        } 
+        // If we have an element with a different type than the previous element
+        else {
+            
+            if(email_cont_array[idx-1].type == 'text') 
+            {
+                final_content += `<tr>
+                <td style="font-family:arial,sans-serif;font-size:15px;color:#575757; padding:30px 30px 0;">${holder}</td></tr>`;
+            } 
+            else 
+            {
+                final_content += `<tr><td style="background-color:#ffffff;"><table border="0"cellpadding="0"cellspacing="0"role="presentation"style="width:620px;"align="left">${holder}</table></td></tr>`;
+            }
+
+            holder = "";
+            holder += el.content;
+        }
+
+        if(idx == email_cont_array.length-1) {
+            if(el.type == 'text') 
+            {
+                final_content += `<tr>
+                <td style="font-family:arial,sans-serif;font-size:15px;color:#575757; padding:30px 30px 0;">${holder}</td></tr>`;
+            } 
+            else 
+            {
+                final_content += `<tr><td style="background-color:#ffffff;"><table border="0"cellpadding="0"cellspacing="0"role="presentation"style="width:620px;"align="left">${holder}</table></td></tr>`;
+            }
+        }
+    });
+
+    // Main container that will hold all email content
+    email_container = `
+    <!doctype html><html><head><meta charset="utf-8"></head><body width="620" style="margin: 0; padding: 0 !important; background: #f3f3f5; mso-line-height-rule: exactly;"><center style="width: 100%; background: #f3f3f5;">${preview_text_container}<div style="width: 620px; margin: 0 auto;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:620px;height:100%">
+    ${data[0].header ? header_image : ''}
+    <tr><td style="background-color:#ffffff;"><table border="0"cellpadding="0"cellspacing="0"role="presentation"style="width:620px;"align="left">
+    ${final_content}
+    </table></td></tr>
+    ${data[0].footer ? footer_image : ''}
+    </table>
+    </div>
+    </center>
+    </body>
+    </html>`;
+
+    $('.preview-container').empty()
+    $('.preview-container').append(`<iframe src="data:text/html;charset=utf-8, ${escape(email_container)}" />`)
+
+    $('.char-count').html(email_container.length);
+}
+
+function generateHtml() {
+    copyToClipboard(email_container);
+    alert('HTML copied to clipboard!');
+}
+
+/**
+ * A helper function to copy the body of the iframe displayed in the page to the user's clipboard
+ * @param {String} text 
+ */
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orignal page when copying more words
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
---
+++ index.js
@@ -0,0 +1,203 @@
+var data = [
+    {
+        header: true,
+        footer: true,
+    }
+]
+var currentPosition = 0;
+$(document).ready(preparePage);
+
+function preparePage() {
+    generateiFrame();
+
+    window.onbeforeunload = function() {
+        return "Data will be lost if you leave the page, are you sure?";
+    };
+
+    // Delete existing form elements by clicking the "x"
+    $('.form-control').on('click', '.delete', function(e) {
+        $(e.currentTarget).parent().closest('.element-container').remove();
+        console.log($(e.currentTarget).parent().data('position'))
+    });
+
+    // Open the popup to add a new form element
+    $('.add-element-btn').click((e) => {
+        $('.form-elements').css('display', 'flex');
+    });
+    $('.form-control').on('click', '.add-element-btn', function(e) {
+        currentPosition = $(e.currentTarget).parent().data('position');
+        console.log('Add button clicked. Current Position: ', currentPosition);
+        $('.form-elements').css('display', 'flex');
+    });
+    $('.element-btn').click((e) => {
+        createFormElement(e)
+    })
+
+    // Close the popup
+    $('.close').click(() => {
+        $('.form-elements').css('display', 'none');
+    });
+
+    $('.btn-preview').click(() => {
+        generateiFrame();
+    });
+
+    $('.btn-html').click(() => {
+        generateHtml();
+    })
+}
+
+function createFormElement(e) {
+    var elementName = $(e.currentTarget).data('name');
+    console.log('Element button clicked. Current Position: ', currentPosition);
+
+    switch (elementName) {
+        case 'title':
+            updatePositions();
+            currentPosition++;
+            var f_Title = `<div class="element-container" data-type="text" data-element="h2" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-title">Title</h2>
+                                <span class="label">TEXT</span>
+                                <input class="input title-text" type="text">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Title).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        case 'heading':
+            updatePositions();
+            currentPosition++;
+            var f_Heading = `<div class="element-container" data-type="text" data-element="h3" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-heading">Heading</h2>
+                                <span class="label">TEXT</span>
+                                <input class="input heading-text" type="text">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Heading).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        case 'paragraph':
+            updatePositions();
+            currentPosition++;
+            var f_Paragraph = `<div class="element-container" data-type="text" data-element="p" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-paragraph">Paragraph</h2>
+                                <span class="label">TEXT</span>
+                                <textarea class="input paragraph-text" type="text"></textarea>
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Paragraph).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        case 'subheading':
+            updatePositions();
+            currentPosition++;
+            var f_Subheading = `<div class="element-container" data-type="text" data-element="h4" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-subheading">Subheading</h2>
+                                <span class="label">TEXT</span>
+                                <input class="input subheading-text" type="text">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Subheading).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        case 'preview':
+            updatePositions();
+            currentPosition++;
+            var f_Preview = `<div class="element-container" data-type="text" data-element="pre" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-preview">Preview Text</h2>
+                                <span class="label">TEXT</span>
+                                <input class="input preview-text" type="text">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Preview).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;        
+        case 'image':
+            updatePositions();
+            currentPosition++;
+            var f_Image = `<div class="element-container" data-type="feature" data-element="img" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-image">Image</h2>
+                                <span class="label">IMAGE SOURCE URL</span>
+                                <input class="input image-url" type="text">
+                                <span class="label">IMAGE TEXT</span>
+                                <input class="input image-text" type="text">
+                                <span class="label">SPACING</span>
+                                <input class="input image-space" type="text" min="15" max="350" value="15">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Image).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        case 'button':
+            updatePositions();
+            currentPosition++;
+            var f_Button = `<div class="element-container" data-type="feature" data-element="button" data-position="${currentPosition}">
+                            <div class="element" data-position="${currentPosition}">
+                                <div class="delete">x</div>
+                                <h2 class="element-title ele-button">Button</h2>
+                                <span class="label">BUTTON TEXT</span>
+                                <input maxlength="21" class="input button-text" type="text">
+                                <span class="label">LINK</span>
+                                <input class="input button-url" type="text">
+                                <span class="label">SPACING</span>
+                                <input class="input button-space" type="number" min="15" max="350" value="15">
+                            </div>
+                            <div class="add-element" data-position="${currentPosition}">
+                                <div class="add-spacer"></div>
+                                <div class="add-element-btn">+</div>
+                                <div class="add-spacer"></div>
+                            </div>
+                        </div>`
+            $(f_Button).insertAfter($(`.element-container[data-position="${currentPosition-1}"]`));
+        break;
+        default:
+        break;
+    }
+}
+
+function updatePositions() {
+    document.querySelectorAll('.element-container').forEach((elem) => {
+        var position = parseInt($(elem).attr('data-position'));
+        if(position > currentPosition) {
+            $(elem).attr('data-position', position+1);
+            $(elem).find('.element').attr('data-position', position+1);
+            $(elem).find('.add-element').attr('data-position', position+1);
+        }
+    })
+}
